7180b95851bfb09ad1731c4054f940df5774d582
friedpanseller <friedpanseller@gmail.com>
2025-07-17 15:00:35 +1000

    Fixed windows paths

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,8 @@ import { APP_DIR, VALID_PAGE_EXTENTIONS } from './config';
 import { FSTreeWalker, Route } from './types';
 
 const cleanPath = (filePath: string, appDir: string = APP_DIR): string => {
-  const sep = path.sep.replace(/\\/g, '\\\\'); // Escape for Windows paths
-  const regex = (pattern: string) => new RegExp(pattern.replaceAll('/', sep), 'g');
+  // const sep = path.sep.replace(/\\/g, '\\\\'); // Escape for Windows paths
+  const regex = (pattern: string) => new RegExp(pattern, 'g');
 
   return filePath
     .replace(appDir, '') // Remove base app directory
@@ -73,8 +73,9 @@ const generateRoutes = async (appDir: string): Promise<Route[]> => {
         }
       }
 
+      // Replace NTFS backslashes with URL forwardslashes
       routes.push({
-        path: base,
+        path: base.replace(/\\/, "/"),
         isDynamic,
         isCatchAll,
         isOptionalCatchAll,
